607b88da9ec1800a39460417c50667e0aacbccda
BigBoySanchez <jacobmath3@gmail.com>
2023-11-07 14:13:22 -0800

    Start Lab6

---
+++ ASM-Stuff/Fall2023/Lab6/Lab6.cpp
@@ -0,0 +1,39 @@
+//
+// Lab6.cpp
+//
+// Jacob Ativo
+// 11/7
+// Using the shifting 
+// multiply method in C++
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int num1, num2, result = 0, check;
+
+    cout << "\nEnter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    //need to do this before shifting 
+    check = num1 * num2;
+
+    while(num1 != 0)
+    {
+        //adds num2 * 2^n iff num1 / 2^n is odd
+        result += num2 * (num1 & 1);
+
+        num1 = num1 >> 1;
+        num2 = num2 << 1;
+    }
+
+    cout << "\nThe result from shifting: " << result;
+    cout << "\nThe real result: " << check << '\n';
+    
+    return 0;
+}
