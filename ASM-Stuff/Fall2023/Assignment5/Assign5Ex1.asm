9f56b88f330f9e0d566d68a33189945910bac5b3
BigBoySanchez <jacobmath3@gmail.com>
2023-11-14 17:01:08 -0800

    1st draft of ex1

--- ASM-Stuff/Fall2023/Assignment5/Assign5Ex1.asm
+++ ASM-Stuff/Fall2023/Assignment5/Assign5Ex1.asm
@@ -8,30 +8,61 @@
 #
 # Settings: Load delays ON; Branch delays ON, Pseudoinstructions ON
 # 
+# Register use:
+# $f0 --- y
+# $f1 --- x
+# $f2 --- 5.4xy
+# $f3 --- -12.3y
+# $f4 --- 18.23x
+# $f12 -- answer
+#
 
         .data
 prX:    .asciiz "\nEnter a value for x: "
-prY:    .asciiz "\nEnter a value for y: "
+prY:    .asciiz "Enter a value for y: "
 ans:    .asciiz "\nThe result is: "
 
         .text
         .globl  main
 
-main:   la      $a0, prX                    # print the x prompt
+main:   la      $a0, prX                # print the x prompt
         li      $v0, 4
         syscall
 
-        li      $v0, 6                      # reads x as a single float
+        li      $v0, 6                  # reads x as a single float
         syscall
-        mov.s   $f1, $f0                    # and moves it to $f1 to make room for y
+        mov.s   $f1, $f0                # and moves it to $f1 to make room for y
 
-        la      $a0, prY                    # print the y prompt
+        la      $a0, prY                # print the y prompt
         li      $v0, 4
         syscall
 
-        li      $v0, 6                      # reads y as a single float
+        li      $v0, 6                  # reads y as a single float
         syscall
 
-exit:   li      $v0, 10
-        syscall          
+        li.s    $f3, 5.4                # loads 5.4 into $f3
+        mul.s   $f2, $f0, $f1           # puts xy into $f2
+        mul.s   $f2, $f2, $f3           # 5.4xy in $f2
+
+        li.s    $f3, -12.3              # loads -12.3 into $f3
+        mul.s   $f3, $f3, $f0           # -12.3y in $f3
+
+        li.s    $f4, 18.23              # loads 18.23 in $f4
+        mul.s   $f4, $f4, $f1           # 18.23x in $f4
+
+        li.s    $f12, -8.23             # adding everything together
+        add.s   $f12, $f12, $f4         # 18.23x - 8.23
+        add.s   $f12, $f12, $f3         # - 12.3y + 18.23x - 8.23
+        add.s   $f12, $f12, $f2         # 5.4xy - 12.3y + 18.23x - 8.23
+
+        la      $a0, ans                # print answer prompt
+        li      $v0, 4
+        syscall
+
+        li      $v0, 2                  # print floating point answer
+        syscall                    
+
+        li      $v0, 10
+        syscall
+# End of file          
 
