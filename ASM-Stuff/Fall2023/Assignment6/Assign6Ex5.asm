7e6b70c7ccffd901383f4b68d46e2b36897ddd12
BigBoySanchez <jacobmath3@gmail.com>
2023-11-26 23:20:29 -0800

    Finish Assign6Ex5

--- ASM-Stuff/Fall2023/Assignment6/Assign6Ex5.asm
+++ ASM-Stuff/Fall2023/Assignment6/Assign6Ex5.asm
@@ -9,9 +9,6 @@
 # Settings: Bare Machine OFF, Allow Pseudo Instructions ON, 
 # 	    Load Exception File OFF, Delayed Branches ON, 
 # 	    Delayed Loads ON, Mapped IO OFF
-# 
-# Register Use:
-# WIP
 #
 
 	.data
@@ -28,17 +25,58 @@ main:	la	$a0, prompt						# print prompt
 	syscall
 
 	la	$t0, arr						# load pointer for arr into $t0
-	li	$t1, 10							# $t1 holds num of remaining inputs
+	addiu	$t1, $t0, 36						# $t1 holds address of the end of arr (add (10-1) * sizeof(int))
 
 	# put ints into arr using loop
-read:	li	$v0, 5							# read user int
+read:	bgt	$t0, $t1, set						# break if $t0 is outside arr
+	li	$v0, 5							# read user int
 	syscall
 
 	sw	$v0, ($t0)						# move int into arr
 	addiu	$t0, $t0, 4						# increment pointer to next int
-	addiu	$t1, $t1, -1						# one less input is needed
-	bnez	$t1, read						# read more if more inputs are needed
+	j	read							# try reading another int
 	nop
+
+set:	la	$t0, arr						# move pointer back to beginning of arr
+	lw	$t2, ($t0)						# will hold biggest int. default is first int in arr
+	lw	$t3, ($t0)						# will hold smallest int. default is first int in arr
+
+	# look for smallest and largest int in the array
+find:	beq	$t0, $t1, done						# break if pointer is at last int in arr
+	addiu	$t0, $t0, 4						# increment pointer to next int
+	lw	$t4, ($t0)						# load int at the pointer temporarily in $t4
+	nop
+	bgt	$t4, $t2, newB						# change biggest int if pointer points to bigger int
+	nop
+	blt	$t4, $t3, newL						# change smallest int if pointer points to smaller int
+	nop
+	j	find							# loop again
+	nop
+
+newB:	move	$t2, $t4						# change biggest int into temp int
+	j	find							# and check next int
+	nop
+
+newL:	move	$t3, $t4						# change smallest int into temp int
+	j	find							# and check next int
+	nop
+
+	#print results
+done:	la	$a0, small						# print small text
+	li	$v0, 4
+	syscall
+
+	move	$a0, $t3						# print smallest int
+	li	$v0, 1
+	syscall
+
+	la	$a0, big						# print big text
+	li	$v0, 4
+	syscall
+
+	move	$a0, $t2						# print biggest int
+	li	$v0, 1
+	syscall
 
 	li	$v0, 10							# exit
 	syscall
