4e62140e81ae3e5e3505b06c8f52ae5d1a8ddd95
BigBoySanchez <jacobmath3@gmail.com>
2023-11-30 01:03:55 -0800

    dude what is happening

--- ASM-Stuff/Fall2023/Assignments/Assignment7/Assign7Ex1.asm
+++ ASM-Stuff/Fall2023/Assignments/Assignment7/Assign7Ex1.asm
@@ -11,17 +11,32 @@
 #
 
         .data
-prompt:	.asciiz	"Enter an integers: "	# prompts user for integer
-arr:    .space	40			# space for 10 ints
+prompt:	.asciiz	"Enter an integer: "	# prompts user for integer
+arr:    .word	40			# space for 10 ints
 
 	.text
 	.globl	main
 
-main:	li	$t0, 10			# store remaining number of ints
-	la	$t1, prompt		# load address of prompt into $t1
+main:	la	$a0, prompt		# $a0 = address of prompt
+	la	$a1, arr		# $a1 = address of arr
+	addiu	$t0, $a1, 36		# $t0 = end of array
 
-subL:	sw	$t0, ($sp)		# push $t0 (for use later)
-	addiu	$sp, $sp, -4		
-	sw	$t1, ($sp)		# pass address of prompt as an argument
-	addiu	$sp, $sp, -4
-	
+subL:	jal	read			# go into read subroutine
+	nop
+	addiu	$a1, $a1, 4
+	ble	$a1, $t0, subL
+	nop
+
+	li	$v0, 10
+	syscall	
+
+
+read:	li	$v0, 4			# print prompt
+	syscall
+
+	li	$v0, 5			# read int into $v0
+	syscall
+
+	sw	$v0, ($a1)
+	jr	$ra			# return control to main
+	nop
