dfbcdd812872d1fce820c62a279bb3e63ef6a2f7
BigBoySanchez <jacobmath3@gmail.com>
2023-12-02 17:56:38 -0800

    finish lab 7

--- ASM-Stuff/Fall2023/Labs/Lab7/Lab7.asm
+++ ASM-Stuff/Fall2023/Labs/Lab7/Lab7.asm
@@ -9,32 +9,132 @@
 # 	    Load Exception File OFF, Delayed Branches ON, 
 # 	    Delayed Loads ON, Mapped IO OFF
 #
-# Register Use:
-# WIP
-#
 
 	.data
-prompt:	.asciiz	"Enter an int (value is at least 1): "	# prompt user for a valid integer
-post:	.asciiz	"SumInts of n is m"			# tells user the result of SumInts
+prompt:	.asciiz	"\n\nEnter int >=1 (<1 to quit): "
+post1:	.asciiz	"SumInts of "				# reasonable descriptive text
+post2:	.asciiz	" is "
 
 	.text
 	.globl	main
-
-main:	la	$a0, prompt				# print prompt
+main:	la	$a0, prompt				# prompt for n
 	li	$v0, 4
 	syscall
 
-	li	$v0, 5					# read user's int into $v0 (assume it's valid)
+	li	$v0, 5					# read int
 	syscall
 
-	sw	$v0, ($sp)				# push user's int
-	addiu	$sp, $sp, -4
-	jal	sumInt					# store return address after sumInt
+	blez	$v0, quit				# quit when n < 1
+	nop
 
+	# caller prolog for main
+	sw	$v0, ($sp)				# push n (parameter for subroutine)
+	addiu	$sp, $sp, -4
 	
-	li	$v0, 10
+	jal	sum					# go into subroutine
+	nop
+
+	addiu	$sp, $sp, 4				# pop n into $t0
+	sw	$t0, ($sp)
+	move	$t1, $v0				# make way for syscall codes
+
+	la	$a0, post1				# print first part of output
+	li	$v0, 4
 	syscall
 
-sumInt:	sw	$ra, ($sp)
+	move	$a0, $t0				# print n
+	li	$v0, 1
+	syscall
+
+	la	$a0, post2				# print next part of output
+	li	$v0, 4
+	syscall
+
+	move	$a0, $t1				# print m
+	li	$v0, 1
+	syscall
+
+	j	main
+	nop
+
+quit:	li	$v0, 10
+	syscall
+
+	# takes sum of squares from 1 to n
+	# sub prolog
+sum:	sw	$ra, ($sp)				# push return address
 	addiu	$sp, $sp, -4
-	
+
+	sw	$fp, ($sp)				# push frame pointer
+	addiu	$sp, $sp, -4
+
+	addiu	$fp, $sp, -4				# need space for n-1
+	move	$sp, $fp				# initialize $sp
+
+	# add n^2 to sum(n-1)
+	# sub body
+	addiu	$sp, $sp, 16				# move sp to n in stack
+	lw	$t0, ($sp)				# load n from stack
+	li	$t1, 1
+	ble	$t0, $t1, epi				# do another call if n > 1
+	li	$v0, 1					# assume sum returns 1
+
+	addiu	$t0, $t0, -1				# $t0 = n - 1
+
+	# caller prolog for sub
+	addiu	$sp, $fp, 4				# move sp to parameter spot
+	sw	$t0, ($sp)				# push parameter
+	addiu	$sp, $sp, -4
+
+	jal	sum					# recursively call sum
+	nop
+
+	addiu	$sp, $sp, 4				# pop n-1 into $t0
+	lw	$t0, ($sp)
+	nop
+
+	addiu	$t0, $t0, 1				# $t0 = n
+	mult	$t0, $t0				# lo = n^2
+	mflo	$t1					# $t1 = n^2
+	addu	$v0, $v0, $t1				# $v0 = return value + n^2
+
+	# sub epilog
+epi:	addiu	$sp, $fp, 8				# pop old frame pointer
+	lw	$fp, ($sp)
+
+	addiu	$sp, $sp, 4				# pop return address
+	lw	$ra, ($sp)
+	nop
+
+	jr	$ra					# go back to caller
+	nop
+
+# NOTES:
+# real world
+# DO NOT USE $ax FOR ARGS
+# ignore overflow
+#
+# PSEUDOCODE:
+# MAIN
+## prompt for n
+## store n
+## call sumSq
+## print "SumInts of n is m"
+## jump to main while needed
+## exit when done
+# END MAIN
+#
+# SUMSQ
+## load n from stack
+## load a = 1
+## IF n<=1
+### jump to return
+## END IF
+## make n-1
+## call sumSq(n-1)
+## load n-1
+## make n
+## make n^2
+## make a = n^2 + sumSq
+## return a
+# END SUMSQ
