01018652b77c5ff821594623e3f0ba4a3db931e5
BigBoySanchez <jacobmath3@gmail.com>
2024-10-25 00:28:58 -0700

    up to q8 on hw4

--- Fall2024/CS-311/HW4/hw4.py
+++ Fall2024/CS-311/HW4/hw4.py
@@ -66,3 +66,63 @@ could contribute to the existence of this.
 
 Imaginary numbers are a rare feature. It's nice seeing them here.
 """
+
+# strings
+
+str = "Hello Worldenheimer"
+print(str)
+print(str + " fjiewj")
+
+try:
+    str[7] = "\n"
+except TypeError:
+    print("can't change string")
+
+"""
+CRITIQUE:
+
+strings function similar to the string class in C++, (append, indexing), but
+they are immutable which is understandable. It just means some algorithms take
+more space to copy one string, but everything else is fine.
+"""
+
+# arrays
+
+import array
+arr = array.array('i', [1, 2, 3])
+
+"""
+CRITIQUE:
+
+hate them in this language. fixed-size like in c/c++, but out of place (need 
+loop to print, need to specify type). Can only hold numbers.
+"""
+
+# lists
+
+lis = [1, 'ni hao', 2, 3, [1, 2, 3]]
+print(lis)
+
+"""
+CRITIQUE:
+
+friggin' awesome! Can hold multiple types at the same time, a bunch of cool methods, like append.
+Similar to vectors in C++, but not restricted to single types. A lil slow, but its Python.
+"""
+
+# tuples
+
+tup = (1, 2, "wow")
+print(tup)
+
+try:
+    tup[0] = 2
+except TypeError:
+    print("can't change tuple")
+
+"""
+CRITIQUE:
+
+can hold many different types and uses indexing, like lists, but every part of
+it is immutable. Similar functionality on C/C++
+"""
